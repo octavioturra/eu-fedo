02d4d1bb238eac4274c7fe8104cd234413c32277
Octavio Turra <octavio@guiabolso.com.br>
2020-02-03 15:42:10 -0300

    :hammer: fix useSimpleForm

--- src/useSimpleForm.ts
+++ src/useSimpleForm.ts
@@ -1,10 +1,7 @@
 import { useState } from "react";
 export default function useSimpleForm<T>(initialState: T | null) {
-  if (!initialState) {
-    throw new Error("Provide an initial sate");
-  }
   const [errors, setErrors] = useState([] as string[]);
-  const [fields, setFields] = useState(initialState);
+  const [fields, setFields] = useState(initialState || {});
 
   function setField(field: string) {
     return (ev: any | undefined) =>
